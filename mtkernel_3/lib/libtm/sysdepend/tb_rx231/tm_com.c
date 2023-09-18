96e9bed394dbdd4ba334ae8380dd30d0f39ce9f8
鹿取 祐二 <94896186+yuji-katori@users.noreply.github.com>
2023-09-18 23:24:43 +0900

    Add files via upload

--- mtkernel_3/lib/libtm/sysdepend/tb_rx231/tm_com.c
+++ mtkernel_3/lib/libtm/sysdepend/tb_rx231/tm_com.c
@@ -32,10 +32,10 @@ EXPORT	void	tm_snd_dat( const UB* buf, INT size )
 #if USE_COM
 int i;
 	for( i=0 ; i < size ; i++ )  {
-		while(!IR(SCI5, TXI5))
+		while(!IR(SCI6, TXI6))
 			;
-		IR(SCI5, TXI5) = 0;
-		SCI5.TDR = buf[i];
+		IR(SCI6, TXI6) = 0;
+		SCI6.TDR = buf[i];
 	}
 #endif /* USE_COM */
 }
@@ -51,13 +51,13 @@ EXPORT	void	tm_rcv_dat( UB* buf, INT size )
 int i;
 	for( i=0 ; i < size ; i++ )
 		while( 1 )  {
-			if(IR(SCI5, RXI5))  {
-				buf[i] = SCI5.RDR;
-				IR(SCI5, RXI5) = 0;
+			if(IR(SCI6, RXI6))  {
+				buf[i] = SCI6.RDR;
+				IR(SCI6, RXI6) = 0;
 				break;
 			}
-			if(SCI5.SSR.BYTE & 0x38)
-				SCI5.SSR.BYTE = 0xC0;
+			if(SCI6.SSR.BYTE & 0x38)
+				SCI6.SSR.BYTE = 0xC0;
 		}
 #endif /* USE_COM */
 }
@@ -68,22 +68,22 @@ EXPORT	void	tm_com_init(void)
 #if USE_COM
 int i;
 	SYSTEM.PRCR.WORD = 0xA502;
-	MSTP( SCI5 ) = 0;
+	MSTP( SCI6 ) = 0;
 	SYSTEM.PRCR.WORD = 0xA500;
 	
 	MPC.PWPR.BIT.B0WI = 0;
 	MPC.PWPR.BIT.PFSWE = 1;
-	MPC.PC3PFS.BIT.PSEL = 0x0A;
-	MPC.PC2PFS.BIT.PSEL = 0x0A;
+	MPC.P32PFS.BIT.PSEL = 0x0B;
+	MPC.P33PFS.BIT.PSEL = 0x0B;
 	MPC.PWPR.BYTE = 0x80;
 
-	// GPIO setting PC3 = TXD5, PC2 = RXD5
-	PORTC.PMR.BIT.B3 = 1;
-	PORTC.PMR.BIT.B2 = 1;
+	// GPIO setting P32 = TXD6, P33 = RXD6
+	PORT3.PMR.BIT.B2 = 1;
+	PORT3.PMR.BIT.B3 = 1;
 
-	// Initialize SCI5
-	SCI5.BRR = PCLKB / (64.0F / 2 * BAUD_RATE) - 0.5F;
+	// Initialize SCI6
+	SCI6.BRR = PCLKB / (64.0F / 2 * BAUD_RATE) - 0.5F;
 	for( i=0 ; i<ICLK/4/BAUD_RATE  ; i++ )  ;
-	SCI5.SCR.BYTE |= 0xF0;
+	SCI6.SCR.BYTE |= 0xF0;
 #endif /* USE_COM */
 }
